4c6ddce7f1e533760f3e59b48ff8e77ef6e395b0
harjot <harjot23062000@gmail.com>
2022-05-11 17:06:40 -0700

    added css to the project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const unsplashURL = 'https://source.unsplash.com/random/'
 const container=document.querySelector('.container')
-const rows=10;
+const rows=13;
 
 for (let i=0; i<rows*3;i++){
     const img=document.createElement('img')
